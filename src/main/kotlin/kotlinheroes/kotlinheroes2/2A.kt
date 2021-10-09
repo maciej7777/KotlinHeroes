7a8ee20a17bdfde5f6cffa8b0759300f6eb35cac
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2021-10-09 18:36:36 +0200

    Add solutions for Kotlin Heroes 2 competition

--- src/main/kotlin/kotlinheroes/kotlinheroes2/2A.kt
+++ src/main/kotlin/kotlinheroes/kotlinheroes2/2A.kt
@@ -1,2 +1,38 @@
 package main.kotlin.kotlinheroes.kotlinheroes2
 
+import java.util.*
+
+class Problem(val complexity: Int, val index: Int)
+
+/**
+ * This is a solution for a problem described in https://codeforces.com/contest/1211/problem/A
+ */
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+
+
+    val problems = mutableListOf<Problem>()
+
+    for (i in 0 until n) {
+        val r = sc.nextInt()
+        problems.add(Problem(r, i))
+    }
+
+    problems.sortBy{it.complexity}
+
+    val solution = mutableListOf<Int>()
+    solution.add(problems[0].index + 1)
+
+    for (i in 1 until n) {
+        if (problems[i-1].complexity < problems[i].complexity) {
+            solution.add(problems[i].index + 1)
+        }
+    }
+
+    if (solution.size < 3) {
+        println("-1 -1 -1")
+    } else {
+        println("" + solution[0] + " " + solution[1] + " " + solution[2])
+    }
+}
--- src/main/kotlin/kotlinheroes/kotlinheroes2/2B.kt
+++ src/main/kotlin/kotlinheroes/kotlinheroes2/2B.kt
@@ -1,2 +1,28 @@
 package main.kotlin.kotlinheroes.kotlinheroes2
 
+import java.util.*
+
+/**
+ * This is a solution for a problem described in https://codeforces.com/contest/1211/problem/B
+ */
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+
+    val integers = Array(n) { 0 }
+    for (i in 0 until n) {
+        integers[i] = sc.nextInt()
+    }
+
+    val max:Long = integers.max()!!.toLong()
+    val maxIndex = integers.indices.maxWith(compareBy(
+        {integers[it]},
+        {it}
+    )) ?: -1
+
+    if (max > 1) {
+        println((max - 1) * n + maxIndex + 1)
+    } else {
+        println(maxIndex + 1)
+    }
+}
